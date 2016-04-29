9efc039520370ad24b2f0b6320a1e7938d229a31
Alexander Sanchez <asanchez.dev@gmail.com>
2016-04-29 14:35:25 -0400

    finished adding the page.c

--- src/vm/page.h
+++ src/vm/page.h
@@ -23,13 +23,22 @@ struct sup_page_entry {
   void *uva;
 };
 
-bool add_file_to_page_table (struct file *file, int32_t ofs, uint8_t *upage,
-                             uint32_t read_bytes, uint32_t zero_bytes,
-                             bool writable);
-bool load_file (struct sup_page_entry *spte);
-bool load_swap (struct sup_page_entry *spte);
-bool load_mmap (struct sup_page_entry *spte);
-void page_table_destroy (struct hash *spt);
-void page_table_init (struct hash *spt);
+bool add_file_to_page_table (struct file*,
+                             int32_t, uint8_t*,
+                             uint32_t, uint32_t,
+                             bool)
+bool load_file (struct sup_page_entry*)
+bool load_swap (struct sup_page_entry*)
+bool load_page (void*)
+bool load_mmap (struct sup_page_entry*)
+void page_table_destroy (struct hash*)
+static unsigned page_hash_func (const struct hash_elem*,
+                                void* e UNUSED) // TODO: might take e out
+static bool page_less_func (const struct hash_elem*,
+                            const struct hash_elem*,
+                            void* aux UNUSED)
+static void page_action_func (struct hash_elem*, void *aux UNUSED)
+void page_table_init (struct hash*)
+static struct sup_page_entry* get_spte (void*)
 
 #endif /* vm/page.h */
