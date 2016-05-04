99ac157bc0dd9a50028b03d5fa66288691fd8500
Oufooz <omarouf96@gmail.com>
2016-05-03 22:31:39 -0400

    update i hope this works

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,45 +1,5 @@
 #include "vm/swap.h"
 
-void swap_init (void)
-{
-  swap_block = block_get_role (BLOCK_SWAP);
-  if (!swap_block)
-    {
-      return;
-    }
-  swap_map = bitmap_create( block_size(swap_block) / SECTORS_PER_PAGE );
-  if (!swap_map)
-    {
-      return;
-    }
-  bitmap_set_all(swap_map, SWAP_FREE);
-  lock_init(&swap_lock);
-}
-
-
-size_t swap_out (void *frame)
-{
-  if (!swap_block || !swap_map)
-    {
-      PANIC("Need swap partition but no swap partition present!");
-    }
-  lock_acquire(&swap_lock);
-  size_t free_index = bitmap_scan_and_flip(swap_map, 0, 1, SWAP_FREE);
-
-  if (free_index == BITMAP_ERROR)
-    {
-      PANIC("Swap partition is full!");
-    }
-
-  size_t i;
-  for (i = 0; i < SECTORS_PER_PAGE; i++)
-    {
-      block_write(swap_block, free_index * SECTORS_PER_PAGE + i,
-		  (uint8_t *) frame + i * BLOCK_SECTOR_SIZE);
-    }
-  lock_release(&swap_lock);
-  return free_index;
-}
 
 void swap_in (size_t used_index, void* frame)
 {
@@ -58,7 +18,42 @@ void swap_in (size_t used_index, void* frame)
   for (i = 0; i < SECTORS_PER_PAGE; i++)
     {
       block_read(swap_block, used_index * SECTORS_PER_PAGE + i,
-		 (uint8_t *) frame + i * BLOCK_SECTOR_SIZE);
+                 (uint8_t *) frame + i * BLOCK_SECTOR_SIZE);
     }
   lock_release(&swap_lock);
+}
+
+size_t swap_out (void *frame)
+{
+  if (!swap_map || !swap_block ) PANIC("Error: No swap partition available.");
+
+  lock_acquire(&swap_lock);
+
+  size_t free_index = bitmap_scan_and_flip(swap_map, 0, 1, SWAP_FREE);
+
+  if (BITMAP_ERROR == free_index) PANIC("Error: Swap partition is full.");
+
+  size_t i;
+  for (i = 0; i < SECTORS_PER_PAGE; i++)
+    block_write(swap_block, free_index * SECTORS_PER_PAGE + i, (uint8_t *) frame + i * BLOCK_SECTOR_SIZE);
+  lock_release(&swap_lock);
+
+  return free_index;
+}
+
+
+void swap_init (void)
+{
+  swap_block = block_get_role (BLOCK_SWAP);
+  if (!swap_block)
+    {
+      return;
+    }
+  swap_map = bitmap_create( block_size(swap_block) / SECTORS_PER_PAGE );
+  if (!swap_map)
+    {
+      return;
+    }
+  bitmap_set_all(swap_map, SWAP_FREE);
+  lock_init(&swap_lock);
 }
